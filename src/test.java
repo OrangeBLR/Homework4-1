90661f4d385083c190b70ee009e00f4d606b5434
OrangeBLR <alex.shepturo@gmail.com>
2021-09-18 18:51:29 +0300

    Lesson 1

--- src/test.java
+++ src/test.java
@@ -1,16 +1,11 @@
+import java.util.Scanner;
+
 public class test {
     public static void main (String[] args) {
-       double PI=3.1415926;
-       double radius=6;
-
-       double circle=2*PI*radius;
-       double area=PI*radius*radius;
-        System.out.print("Circle: ");
-        System.out.print(circle);
-
-        System.out.print(" Area: ");
-        System.out.println(area);
-
-        System.out.print(circle>5 || area<125);
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Введите число");
+        int number=scanner.nextInt();
+        System.out.print("Введённое число ");
+        System.out.print(number);
     }
 }
