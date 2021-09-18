2fff1773b5f871ed4841bdc38a2c628b50f61e78
OrangeBLR <alex.shepturo@gmail.com>
2021-09-18 17:28:49 +0300

    Lesson 1

---
+++ src/test.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void main (String[] args) {
+       double PI=3.1415926;
+       double radius=6;
+
+       double circle=2*PI*radius;
+       double area=PI*radius*radius;
+        System.out.print("Circle: ");
+        System.out.print(circle);
+
+        System.out.print(" Area: ");
+        System.out.println(area);
+
+        System.out.print(circle>5 && area<125);
+    }
+}
