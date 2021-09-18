99a3141ee10f6c9aeae51189c973a5ab3c1326c2
OrangeBLR <alex.shepturo@gmail.com>
2021-09-18 19:56:30 +0300

    Homework 4

--- src/test.java
+++ src/test.java
@@ -4,7 +4,6 @@ public class test {
     public static void main (String[] args) {
         Scanner scanner=new Scanner(System.in);
         int number=scanner.nextInt();
-        int num2 = number>0 ? number>100 ? 100 : number : -number;
         System.out.println(num2);
         }
     }
