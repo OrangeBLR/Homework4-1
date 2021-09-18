cb3adf86b13c05e1273ed2960045f21968b7d447
OrangeBLR <alex.shepturo@gmail.com>
2021-09-18 20:02:46 +0300

    Homework 4

--- src/test.java
+++ src/test.java
@@ -5,9 +5,9 @@ public class test {
         Scanner scanner=new Scanner(System.in);
         int number=scanner.nextInt();
         if ( number % 2 == 0 )
-            System.out.println("Число четное");
+            System.out.println("Четное");
         else
-            System.out.println("Число нечетное");
+            System.out.println("Нечетное");
         }
     }
 
