5b2e901f8b2170636c9d7e1e635b6232e0282063
OrangeBLR <alex.shepturo@gmail.com>
2021-09-18 19:51:02 +0300

    Lesson 1

--- src/test.java
+++ src/test.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class test {
     public static void main (String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Введите число");
         int number=scanner.nextInt();
-        System.out.print("Введённое число ");
-        System.out.print(number);
+        int num2 = number>0 ? number>100 ? 100 : number : -number;
+        System.out.println(num2);
+        }
     }
-}
+
